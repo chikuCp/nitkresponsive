c288f45815eb51e3bee25bb2821dc27e44dfe04a
Chander Prakash <Chander_11510402@nitkkr.ac.in>
2018-09-29 12:23:25 +0530

    Basic Layout for Mobile site created
    Added javascript and css for basic design of site for mobiles only

---
+++ javascript/script.js
@@ -0,0 +1,43 @@
+// craete slide show carousel
+
+var myIndex= 0;
+carousel();
+ function carousel(){
+     var i;
+     var x = document.getElementsByClassName("mySlides");
+     for(i=0;i<x.length;i++){
+         x[i].style.display ="none";
+     }
+     myIndex++
+     if(myIndex>x.length){myIndex=1}
+     x[myIndex-1].style.display = "block";
+     setTimeout(carousel,3000)
+ }
+
+
+
+ var coll = document.getElementsByClassName("collapsible");
+ var i;
+ 
+ for (i = 0; i < coll.length; i++) {
+   coll[i].addEventListener("click", function() {
+     this.classList.toggle("active");
+     var content = this.nextElementSibling;
+     if (content.style.display === "block") {
+       content.style.display = "none";
+     } else {
+       content.style.display = "block";
+     }
+   });
+ }
+
+
+ function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+ 
